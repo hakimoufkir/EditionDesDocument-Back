00a783a04bb39a2f7cb094f726a7e00260e0f323
Abdelhakim OUFKIR <abdelhakimoufkir2000@gmail.com>
2024-08-14 14:59:27 +0100

    committing before pulling

---
+++ Domain/Entities/StaticTrainee.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Entities;
+
+public static class StaticTrainee
+{
+    public static List<Trainee> Trainees  { get; private set; } = new();
+    public static TaskCompletionSource Loading = new();
+    
+    public static void SetAndResetList(List<Trainee> products)
+    {
+        Trainees = new List<Trainee>();
+        Trainees.AddRange(products);
+        Loading.TrySetResult(); 
+    }
+
+
+    public static void ResetLoading()
+    {
+        Loading = new TaskCompletionSource();
+    }
+}
